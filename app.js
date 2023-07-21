72fda7210efc3f459f9da5f8817de5a730776867
uk <ukiiing00@gmail.com>
2023-07-21 11:35:09 +0900

    addEventListener : event Object argument / event.preventDefault

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert('Hello');
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
